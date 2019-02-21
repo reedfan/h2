fa5a1d0e7490e06ac62b213293e6725ade5b107c
“qufan” <qufan@cisco.com>
2019-02-21 15:51:43 +0800

    br1

---
+++ cmake-build-debug/CMakeFiles/h2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/h2.dir/main.cpp.o"
+  "h2.pdb"
+  "h2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/h2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
